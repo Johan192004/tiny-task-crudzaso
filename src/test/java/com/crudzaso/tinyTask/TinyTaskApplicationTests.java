44c527012da0701f99b4025e7aab64fb2465a36a
Johan192004 <johanruma@gmail.com>
2025-10-23 09:57:38 -0500

    Initial spring structure

---
+++ src/test/java/com/crudzaso/tinyTask/TinyTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudzaso.tinyTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
